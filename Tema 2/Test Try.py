1b96f61c5721f5ca006c31830245b73b084e7466
Apolyus <baraitarurazvanionut@gmail.com>
2023-03-01 16:02:02 +0200

    Tema 2

---
+++ Tema 2/Test Try.py
@@ -0,0 +1,17 @@
+How_many_tries = 3
+try_count = 0
+while try_count < How_many_tries:
+    try:
+        age = int(input("Put your age :"))
+    except ValueError:
+        print("Invalid data value")
+        try_count = try_count + 1
+        print(f"You have {How_many_tries-try_count} chances remaining")
+        continue
+    if int(age) < 18:
+        print("Minor")
+    else:
+        print("Major")
+    break
+else:
+    print("You have exceeded the maximum amount of tries.")
