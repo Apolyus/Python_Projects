6b277412dcd956d8b7569fafdbd84f042472b1ac
Apolyus <baraitarurazvanionut@gmail.com>
2023-03-04 21:41:56 +0200

    Tema 2 Refacuta

--- Tema 2/Test Try.py
+++ Tema 2/Test Try.py
@@ -1,12 +1,12 @@
-How_many_tries = 3
+how_many_tries = 3
 try_count = 0
-while try_count < How_many_tries:
+while try_count < how_many_tries:
     try:
         age = int(input("Put your age :"))
     except ValueError:
         print("Invalid data value")
         try_count = try_count + 1
-        print(f"You have {How_many_tries-try_count} chances remaining")
+        print(f"You have {how_many_tries-try_count} chances remaining")
         continue
     if int(age) < 18:
         print("Minor")
