eeb2dcfcef36c4a747778a343a1ffa4b6cb549dc
Apolyus <baraitarurazvanionut@gmail.com>
2023-04-02 19:29:07 +0300

    cars in json

--- homework_6_cars/main.py
+++ homework_6_cars/main.py
@@ -1,3 +1,6 @@
+import json
+
+
 if __name__ == '__main__':
     #
     cars_key = ["id", "brand", "model", "horse power", "price"]
@@ -8,8 +11,8 @@ if __name__ == '__main__':
         [3, "Dacia", "Solenza", 100, 900],
         [4, "BMW", "E46", 105, 900],
         [5, "Opel", "Corsa", 130, 4000],
-        [6, "Toyota", "Supra", 190, 6000]
-
+        [6, "Toyota", "Supra", 190, 6000],
+        [8, "Dacia", "Duster", 120, 950]
     ]
 
     last_car_list = [
@@ -30,4 +33,30 @@ if __name__ == '__main__':
     print(sort_by_hp2)
     sort_by_hp3 = [hp for hp in last_car_list if 120 < hp.get("horse power", 0) < 180]
     print(sort_by_hp3)
+
+    map_my_cars = {brand: [car for car in last_car_list if car['brand'] == brand] for brand in
+                   set(car['brand'] for car in last_car_list)
+                   }
+    matching_cars = [car for car in last_car_list if car['brand'] == 'Dacia']
+
+    with open("cheap_cars.json", "w") as file:
+        json.dump(sort_by_price1, file, indent=2)
+
+    with open("medium_cars.json", "w") as file:
+        json.dump(sort_by_price2, file, indent=2)
+
+    with open("expensive_cars.json", "w") as file:
+        json.dump(sort_by_price3, file, indent=2)
+
+    with open("slow_cars.json", "w") as file:
+        json.dump(sort_by_hp1, file, indent=2)
+
+    with open("fast_cars.json", "w") as file:
+        json.dump(sort_by_hp2, file, indent=2)
+
+    with open("slow_cars.json", "w") as file:
+        json.dump(sort_by_hp3, file, indent=2)
+
+    with open("Dacia.json", "w") as file:
+        json.dump(matching_cars, file, indent=2)
 
