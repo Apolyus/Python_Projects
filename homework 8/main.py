bffa25f95bcdb409632a5181ba7c2f9053a213dc
Apolyus <baraitarurazvanionut@gmail.com>
2023-04-10 17:32:22 +0300

    Class Fraction

--- homework 8/main.py
+++ homework 8/main.py
@@ -4,27 +4,30 @@ class Fractions:
         self.nominator = nominator
 
     def __str__(self):
-        return self.numerator, self.nominator
+        return f"{self.numerator}/{self.nominator}"
 
     def __add__(self, other1, other2):
         numerator = (self.numerator * other2) + (other1 * self.nominator)
         nominator = self.nominator * other2
-        return f"{numerator}/{nominator}"
+        return Fractions(numerator, nominator)
 
     def __sub__(self, other1, other2):
         numerator = (self.numerator * other2) - (other1 * self.nominator)
         nominator = self.nominator * other2
-        return f"{numerator}/{nominator}"
+        return Fractions(numerator, nominator)
 
     def __invert__(self):
-
-        return f"{self.nominator}/{self.numerator}"
+        return Fractions(self.nominator, self.numerator)
 
 
 if __name__ == '__main__':
     fractions = Fractions(3, 2)
     print("The numbers are :", fractions.__str__())
+    print("Type : ", type(fractions.__str__()))
     print("Addition : ", fractions.__add__(2, 2))
+    print("Type : ", type(fractions.__add__(2, 2)))
     print("Subtraction : ", fractions.__sub__(2, 2))
+    print("Type : ", type(fractions.__sub__(2, 2)))
     print("The inverted fraction is :", fractions.__invert__())
+    print("Type : ", type(fractions.__invert__()))
 
