d561c9cce4ee0f0721d612aa863dbe701aecc8ab
Apolyus <baraitarurazvanionut@gmail.com>
2023-04-11 18:20:50 +0300

    Class Fraction

--- homework 8/main.py
+++ homework 8/main.py
@@ -1,4 +1,4 @@
-class Fractions:
+class Fraction:
     def __init__(self, numerator, nominator):
         self.numerator = numerator
         self.nominator = nominator
@@ -9,25 +9,25 @@ class Fractions:
     def __add__(self, other1, other2):
         numerator = (self.numerator * other2) + (other1 * self.nominator)
         nominator = self.nominator * other2
-        return Fractions(numerator, nominator)
+        return Fraction(numerator, nominator)
 
     def __sub__(self, other1, other2):
         numerator = (self.numerator * other2) - (other1 * self.nominator)
         nominator = self.nominator * other2
-        return Fractions(numerator, nominator)
+        return Fraction(numerator, nominator)
 
     def __invert__(self):
-        return Fractions(self.nominator, self.numerator)
+        return Fraction(self.nominator, self.numerator)
 
 
 if __name__ == '__main__':
-    fractions = Fractions(3, 2)
-    print("The numbers are :", fractions.__str__())
-    print("Type : ", type(fractions.__str__()))
-    print("Addition : ", fractions.__add__(2, 2))
-    print("Type : ", type(fractions.__add__(2, 2)))
-    print("Subtraction : ", fractions.__sub__(2, 2))
-    print("Type : ", type(fractions.__sub__(2, 2)))
-    print("The inverted fraction is :", fractions.__invert__())
-    print("Type : ", type(fractions.__invert__()))
+    Fraction = Fraction(3, 2)
+    print("The numbers are :", Fraction.__str__())
+    print("Type : ", type(Fraction.__str__()))
+    print("Addition : ", Fraction.__add__(2, 2))
+    print("Type : ", type(Fraction.__add__(2, 2)))
+    print("Subtraction : ", Fraction.__sub__(2, 2))
+    print("Type : ", type(Fraction.__sub__(2, 2)))
+    print("The inverted fraction is :", Fraction.__invert__())
+    print("Type : ", type(Fraction.__invert__()))
 
